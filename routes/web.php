cac4ca7e442c7a066f31eba4041319a1af22e0b9
matthewgwapo21 <adrianmatthewcortes1@gmail.com>
2026-03-28 18:44:26 +0800

    Last

--- routes/web.php
+++ routes/web.php
@@ -54,27 +54,15 @@ Route::middleware('guest')->group(function () {
 });
 
 // Admin routes
-Route::middleware(['auth', 'admin'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('dashboard');
-
     Route::get('/users', [\App\Http\Controllers\Admin\AdminUserController::class, 'index'])->name('users');
-
-    Route::put('/users/{user}/promote', [\App\Http\Controllers\Admin\AdminUserController::class, 'promote'])->name('users.promote');
-
-    Route::put('/users/{user}/demote', [\App\Http\Controllers\Admin\AdminUserController::class, 'demote'])->name('users.demote');
-
     Route::delete('/recipes/{recipe}', [\App\Http\Controllers\Admin\AdminRecipeController::class, 'destroy'])->name('recipes.destroy');
-
     Route::get('/categories', [\App\Http\Controllers\Admin\AdminCategoryController::class, 'index'])->name('categories.index');
-
     Route::post('/categories', [\App\Http\Controllers\Admin\AdminCategoryController::class, 'store'])->name('categories.store');
-
     Route::put('/categories/{category}', [\App\Http\Controllers\Admin\AdminCategoryController::class, 'update'])->name('categories.update');
-
+    Route::put('/users/{user}/promote', [\App\Http\Controllers\Admin\AdminUserController::class, 'promote'])->name('admin.users.promote');
+    Route::put('/users/{user}/demote', [\App\Http\Controllers\Admin\AdminUserController::class, 'demote'])->name('admin.users.demote');
     Route::delete('/categories/{category}', [\App\Http\Controllers\Admin\AdminCategoryController::class, 'destroy'])->name('categories.destroy');
 });
 
